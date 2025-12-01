7271fae1574c993b624cf952dbd4a62523e9db32
Josh Buckland <josh.russell.buckland@gmail.com>
2025-12-01 15:01:41 +0000

    chore(day-1): get dialcode working

--- src/aoc/day1.py
+++ src/aoc/day1.py
@@ -1,2 +1,47 @@
+from typing import Callable
+from dataclasses import dataclass
+import operator
+
+DIAL_SIZE = 100
+
+
+@dataclass
+class Instruction:
+    name: str
+    operation: Callable[[int, int], int]
+    amount: int
+
+
+def parse_instruction(instruction: str) -> Instruction:
+    direction, amount = instruction[0], int(instruction[1:])
+
+    if direction == "L":
+        return Instruction(name=instruction, operation=operator.sub, amount=amount)
+    elif direction == "R":
+        return Instruction(name=instruction, operation=operator.add, amount=amount)
+    else:
+        raise ValueError()
+
+
+def run_instruction(start_position: int, instruction: Instruction) -> int:
+    return instruction.operation(start_position, instruction.amount) % DIAL_SIZE
+
+
+def turn_dial(start_position: int, instructions: list[str], verbose=False) -> int:
+    current = start_position
+    zero_count = 0
+    print(f"Starting at {current}")
+    for i in instructions:
+        instruction = parse_instruction(i)
+        next = run_instruction(current, instruction)
+        if verbose:
+            print(f"The dial is rotated {instruction.name} to point at {next}")
+        if next == 0:
+            zero_count += 1
+        current = next
+
+    return zero_count
+
+
 def run_day1():
     print("🎄 Running Day 1")
--- tests/test_day1.py
+++ tests/test_day1.py
@@ -1,1 +1,22 @@
-from aoc.day1 import run_day1
+import pytest
+from aoc.day1 import turn_dial
+
+
+@pytest.fixture
+def mock_instructions() -> list[str]:
+    return [
+        "L68",
+        "L30",
+        "R48",
+        "L5 ",
+        "R60",
+        "L55",
+        "L1 ",
+        "L99",
+        "R14",
+        "L82",
+    ]
+
+
+def test_turn_dial(mock_instructions):
+    assert turn_dial(50, mock_instructions, verbose=True) == 3
